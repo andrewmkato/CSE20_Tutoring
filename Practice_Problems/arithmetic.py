1d90b4eb73830067cf6509b396c9eecd51072cb3
Andrew Kato <andrewmkato@gmail.com>
2022-11-28 07:14:32 -0800

    Added additional problems on arithmetic.

---
+++ Practice_Problems/arithmetic.py
@@ -0,0 +1,20 @@
+# One of the simpler yet fundamental usages of a computer programming languages
+#   is for arithmetic equations. Python is like a personal calculator in that
+#   you can do basic calculations, and build more complex ones. Calculus,
+#   statistics, and other advanced mathematics can be done all in Python. These
+#   problems are meant to engage a basic understanding of how calculations are
+#   done in the language, as well as how operator precedence works.
+
+# TODO: What would the value of the variable below be if you were to print it
+#       out? Give it a try? Change the symbols and see what happens!
+
+x = 9 + (13 - 20) + 2
+# print(x)
+
+
+# TODO: Fix the commented-out equations below so that Python won't throw an 
+#       error if run. Are the symbols correct? The goal is to have answer1 equal
+#       81. Next, answer2 should equal 1.
+
+# answer1 = 9 x 9
+# answer2 = 2^4 - 15
---
+++ Practice_Problems/operator_precedence.py
@@ -0,0 +1,22 @@
+# For the problems below, think about PEMDAS and how code should generally
+#   incorporate parentheses for readability.
+
+# TODO: Rewrite the equation below so it better reflects the order of operations.
+
+# This code is valid in Python, and it will still run, but it is unnecessarily
+#   unreadable. Edit it however you think would perhaps mitigate this.
+equation1 = 2**5 -17- (20 + 1)*2/ 1
+
+
+# TODO: What will the following equation display when printed? Feel free to edit.
+
+equation2 = ((3)**(3-1)) + 2
+# print(equation2)
+
+# TODO: Even if you aren't quite sure what exactly the following equation does,
+#       what do you think it means. Try breaking it down into smaller parts or
+#       talking through it. Remember, it is a list, so it must contain at least
+#       something.
+
+equation3 = [number for number in range(0, 100) if number % 2 == 0]
+# print(equation3)
