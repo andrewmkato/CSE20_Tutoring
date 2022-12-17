59a6c7e4abecd990a3cbc8d00b2d83cd84d0845f
Andrew Kato <andrewmkato@gmail.com>
2022-12-17 03:37:42 -0800

    Created slicing problems.

---
+++ Practice_Problems/string_slicing.py
@@ -0,0 +1,20 @@
+# Think of slicing like grabbing a piece of a larger cake, except that piece
+# is a copy rather than the real thing. In Python, strings are immutable, but
+# you can pull out copies of string slices to manipulate them as needed.
+
+# TODO: Use slice notation to assign to a new variable the first half of
+#       the following string.
+
+original_string = "loremipsumdolorsitamet"
+
+
+# TODO: Now, reassign to that variable every third letter of original_string.
+
+
+# TODO: Create a new variable, odd_nums, and assign to it a string containing
+#       only the numbers in num_string that are odd. Plan how you might do this.
+#       How would you be able to test each character in the string as a number?
+
+num_string = "28393745129381"
+
+# odd_nums should equal "39375931"
