956bf5576e47dbad27d9903814e92ffbfdfe526d
Andrew Kato <andrewmkato@gmail.com>
2022-12-17 16:46:41 -0800

    Answers added for string-slicing.

--- Practice_Problem_Answers/string_slicing_ans.py
+++ Practice_Problem_Answers/string_slicing_ans.py
@@ -14,6 +14,7 @@ print(new_version)
 # TODO: Now, reassign to that variable every third letter of original_string.
 
 new_version = original_string[::3]
+print(new_version)
 
 # TODO: Create a new variable, odd_nums, and assign to it a string containing
 #       only the numbers in num_string that are odd. Plan how you might do this.
@@ -23,7 +24,13 @@ num_string = "28393745129381"
 
 # odd_nums should equal "39375931"
 
-num_string = list(num_string)
-print(int(num_string[0]))
-odd_nums = [num for num in num_string if int(num)/2 != 0]
-print(odd_nums)
+num_string = list(num_string) # Typecasting the string into a list.
+
+# Using a list comprehension to filter out even numbers.
+odd_collection = [num for num in num_string if int(num)%2 != 0]
+
+odd_nums = "" # String that will take on each string-number from the collection.
+for i in odd_collection:
+    odd_nums += i # Adding each string-number to the list.
+
+print(odd_nums) # "39375931"
